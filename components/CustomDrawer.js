04cb0904f3ec83faedd18f6d24650a5cd021e498
JeongGoEun <wjdrhdms002@naver.com>
2020-07-27 15:24:11 +0900

    Update header ui

--- components/CustomDrawer.js
+++ components/CustomDrawer.js
@@ -1,58 +1,74 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image, TouchableOpacity, Text } from 'react-native';
 import { Icon, Button } from 'react-native-elements';
 
-import {} from '../images/real_estate.jpg'
+import { } from '../images/real_estate.jpg'
 
 const CustomDrawer = ({ navigation }) => {
     return (
-        <View style={{flex: 1}}>
-            <View style={{flex: 1, alignItems: 'center'}}>
-            <Image style={{
-                width: 300,
-                height: 300,
-                aspectRatio: 0.8,
-                resizeMode: 'contain',
-              }}
-              source={require('../images/real_estate.jpg')} />
+        <View style={{ flex: 1 }}>
+            <View style={{ flex: 1, alignItems: 'center' }}>
+                <Image style={{
+                    width: 300,
+                    height: 200,
+                    aspectRatio: 2,
+                    resizeMode: 'contain',
+                }}
+                    source={require('../images/real_estate.jpg')} />
             </View>
-            <View style={{ flex:2, padding: 20, paddingHorizontal:30 }}>
-                <Button
-                   icon={{
-                    type:'ionicon',
-                    name: "calculator",
-                    size: 20,
-                    color: "white"
-                  }}
-                    title="부동산 계산기"
+            <View style={{ flex: 1, padding: 20, paddingHorizontal: 30 }}>
+                <TouchableOpacity
+                    style={{height: 48, justifyContent: 'center', marginBottom: 10}}
                     onPress={() => navigation.navigate('CalcPage')}
-                    buttonStyle={styles.btnContainer}
-                    type="solid"
-                />
-                <Button
-                    icon={{
-                        type:'ionicon',
-                        name: "home",
-                        size: 20,
-                        color: "white"
-                      }}
-                    title="내 집 마련"
+                >
+                    <View style={{ flexDirection: 'row' }}>
+                        <Icon
+                            type='ionicon'
+                            name='calculator'
+                            size={20}
+                            color='black'
+                        />
+                        <Text style={{fontSize: 17}}> 부동산 계산기</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={{height: 48, justifyContent: 'center', marginBottom: 10}}
                     onPress={() => navigation.navigate('HousePage')}
-                    buttonStyle={styles.btnContainer}
-                    type="solid"
-                />
-                <Button
-                    icon={{
-                        type:'ionicon',
-                        name: "newspaper",
-                        size: 20,
-                        color: "white"
-                      }}
-                    title="부동산 뉴스"
+                >
+                    <View style={{ flexDirection: 'row' }}>
+                        <Icon
+                            type='ionicon'
+                            name='home'
+                            size={20}
+                            color='black'
+                        />
+                        <Text style={{fontSize: 17}}> 내 집 마련</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={{height: 48, justifyContent: 'center', marginBottom: 10}}
                     onPress={() => navigation.navigate('NewsPage')}
-                    buttonStyle={styles.btnContainer}
-                    type="solid"
-                />
+                >
+                    <View style={{ flexDirection: 'row' }}>
+                        <Icon
+                            type='ionicon'
+                            name='calculator'
+                            size={20}
+                            color='black'
+                        />
+                        <Text style={{fontSize: 17}}> 부동산 뉴스</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+
+            <View style={{ flex: 1, alignItems: 'center',  }}>
+                <Image style={{
+                    //width: 300,
+                    height: 250,
+                    aspectRatio: 0.8,
+                    resizeMode: 'contain',
+                }}
+                    source={require('../images/kbds_logo.jpg')} />
             </View>
         </View>
     );
@@ -61,7 +77,7 @@ const CustomDrawer = ({ navigation }) => {
 const styles = StyleSheet.create({
     btnContainer: {
         marginBottom: 10,
-        backgroundColor: '#FFBC00'
+        //backgroundColor: ''
     }
 });
 
