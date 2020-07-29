46f8a6d66d83d91bd38b4d60a976fd3cd582008d
CoolingSnow <dhehdgus512@naver.com>
2020-07-29 16:09:59 +0900

    200729|modify house part design|Donghyeon

--- pages/House/House_Date.js
+++ pages/House/House_Date.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity, Image } from 'react-native';
 
 const House_Date = ({ navigation }) => {
     const [date_data, setValues] = useState ({
@@ -15,7 +15,7 @@ const House_Date = ({ navigation }) => {
             for(var i = 0; i<text.length; i++){
                 if(numbers.indexOf(text[i])>-1){
                     newText = newText + text[i];
-             b   }
+                }
                 else{
                     alert("please enter numbers only");
                 }
@@ -65,8 +65,14 @@ const House_Date = ({ navigation }) => {
 
     return (
         <View style={styles.inputContainer}>
+            <View style={{alignItems: "center", paddingTop: 10}}>
+                <Image
+                    source={require('../../images/img_home_date.jpg')}
+                    style={{ width: 240, height: 240, resizeMode: 'contain', borderRadius: 15 }}
+                />
+            </View>
             <View style={styles.container}>
-                <Text>목표금액</Text>
+                <Text style = {styles.text_container1}>목표금액</Text>
                 <TextInput
                     style={styles.input}
                     placeholder="0"
@@ -75,10 +81,10 @@ const House_Date = ({ navigation }) => {
                     value = {date_data.amount_wanted}
                     onChangeText = {onChange_amount_wanted}
                 />
-                <Text>만원</Text>
+                <Text style = {styles.text_container}>만원</Text>
             </View>
             <View style={styles.container}>
-                <Text>투자가능금액(월)</Text>
+                <Text style = {styles.text_container2}>투자가능금액(월)</Text>
                 <TextInput
                     style={styles.input}
                     placeholder="0"
@@ -87,10 +93,10 @@ const House_Date = ({ navigation }) => {
                     value = {date_data.amount_investment}
                     onChangeText = {onChange_amount_investment}
                 />
-                <Text>만원</Text>
+                <Text style = {styles.text_container}>만원</Text>
             </View>
             <View style={styles.container}>
-                <Text>보유금액(대출포함)</Text>
+                <Text style = {styles.text_container3}>보유금액(대출포함)</Text>
                 <TextInput
                     style={styles.input}
                     placeholder="0"
@@ -99,48 +105,75 @@ const House_Date = ({ navigation }) => {
                     value = {date_data.amount_holding}
                     onChangeText = {onChange_amount_holding}
                 />
-                <Text>만원</Text>
+                <Text style = {styles.text_container}>만원</Text>
             </View>
-            
-            <View style={styles.button}>
-                <Button title={'분석하기'} onPress={() => navigation.navigate('House_Date_Result_Page', date_data)}/>
-            </View>
+            <TouchableOpacity onPress={() => navigation.navigate('House_Date_Result_Page', date_data)}>
+                <View style={styles.button}>
+                    <Text style={styles.buttonText}>분석하기</Text>
+                </View>          
+            </TouchableOpacity> 
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-  inputContainer: {
-    paddingTop: 60,
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  container: {
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  button: {
-    marginTop: 30,
-    marginRight: 10,
-    marginLeft: 10,
-    marginBottom: 30,
-    width: 100,
-    alignItems: 'center',
-  },
-  buttonText: {
-    textAlign: 'center',
-    paddingRight: 60,
-    paddingLeft: 60,
-    paddingTop: 10,
-    paddingBottom: 10,
-    color: 'white'
-  },
-  input: {
-      borderBottomColor: '#bbb',
-      borderBottomWidth: 1,
-      fontSize: 24,
-      marginLeft: 20,
-  }
+    inputContainer: {
+        paddingTop: 60,
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    container: {
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#FFBC00',
+        borderRadius: 10,
+        marginTop: 30,
+    },
+    buttonText: {
+        textAlign: 'center',
+        paddingRight: 30,
+        paddingLeft: 30,
+        paddingTop: 10,
+        paddingBottom: 10,
+        color: 'white',
+        fontSize: 16,
+    },
+    input: {
+        borderWidth: 2,
+        backgroundColor: 'white',
+        borderColor: '#FFBC00',
+        borderRadius: 8,
+        paddingLeft: 10,
+        paddingRight: 10,
+        height: 45,
+        width: 120,
+        fontSize: 20,
+        textAlign: 'right',
+    },
+    text_container: {
+        margin: 20,
+    },
+    text_container1: {
+        marginTop: 20,
+        marginBottom: 20,
+        marginRight: 10,
+        marginLeft: 55,
+    },
+    text_container2: {
+        marginTop: 20,
+        marginBottom: 20,
+        marginRight: 10,
+        marginLeft: 10,
+    },
+    text_container3: {
+        marginTop: 20,
+        marginBottom: 20,
+        marginRight: 10,
+        marginLeft: 0,
+    }
 });
 
 export default House_Date;
